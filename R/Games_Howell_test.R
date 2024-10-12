4860dcde899eb08feb1ce2a9a1e4f6715c89c26b
jklai <p10911004@gmail.com>
2024-10-12 23:23:00 +0800

    init

---
+++ R/Games_Howell_test.R
@@ -0,0 +1,159 @@
+# Games-Howell test
+
+
+
+Games_Howell_test <- function(
+        data,
+        formula,
+        alpha = 0.05,
+        p_adjust_method = "none",
+        descending = TRUE
+){
+    if (!isTRUE(descending) & !isFALSE(descending)) descending <- TRUE
+    p_adjust_method <- match.arg(p_adjust_method, stats::p.adjust.methods)
+
+    y_name <- as.character(formula)[2]
+    x_name <- as.character(formula)[3]
+
+    df0 <- data.frame(
+        y = data[[y_name]],
+        x = data[[x_name]]
+    )
+
+    df0 <- df0[!is.na(df0$y), ]
+
+    # Descriptive ====
+    desc_mat <- with(
+        data = df0,
+        expr = sapply(
+            X = c("length", "mean", "sd", "median", "min", "max"),
+            FUN = function(fns) tapply(y, x, fns)
+        )
+    )
+
+    desc_mat <- desc_mat[order(desc_mat[, "mean"], decreasing = descending), ]
+
+    group_means <- desc_mat[, "mean"]
+    group_sizes <- desc_mat[, "length"]
+    group_names <- names(group_means)
+    group_vars <- desc_mat[, "sd"] ^ 2
+
+    if (length(unique(group_vars)) == 1){
+        if (length(unique(group_sizes)) == 1)
+            warning("Balanced data, please consider Tukey-HSD test.")
+        if (length(unique(group_sizes)) > 1)
+            warning("Balanced data, please consider Tukey-Kramer test.")
+    }
+
+    # # ANOVA model ====
+    # aov_mod <- stats::aov(formula = y ~ x, data = df0)
+
+    # # Degree of freedom (within group) ====
+    # DFerror <- stats::df.residual(aov_mod)
+
+    # # Mean square error ====
+    # MSE <- sum(stats::residuals(aov_mod) ^ 2) / DFerror
+
+    # Labels comparison ====
+    group_comparisons <- outer2(group_names, function(x1, x2) paste(x1, x2, sep = " |vs| "))
+
+    # Differences of each comparison ====
+    group_diff <- outer2(group_means, "-")
+
+    # Standard error ====
+    .calc_group_SE <- function(x1, x2){
+        var1 <- group_vars[x1]
+        var2 <- group_vars[x2]
+        n1 <- group_sizes[x1]
+        n2 <- group_sizes[x2]
+        res <- sqrt( (var1 / n1 + var2 / n2) / 2 )
+        return(res)
+    }
+    group_SE <- outer2(group_names, .calc_group_SE)
+
+    # Pooled degree of freedom ====
+    .calc_pooled_df <- function(x1, x2){
+        var1 <- group_vars[x1]
+        var2 <- group_vars[x2]
+        n1 <- group_sizes[x1]
+        n2 <- group_sizes[x2]
+        numerator <- ( (var1 / n1) + (var2 / n2) ) ^ 2
+        denominator <- ( ((var1 / n1) ^ 2) / (n1 - 1) ) + ( ((var2 / n2) ^ 2) / (n2 - 1) )
+        res <- numerator / denominator
+        return(res)
+    }
+    group_pooled_df <- outer2(group_names, .calc_pooled_df)
+
+    # q-values (Studendized range) ====
+    group_qvals <- abs(group_diff / group_SE)
+
+    # p-values ====
+    group_pvals <- stats::ptukey(
+        q = group_qvals,
+        nmeans = length(group_names),
+        df = group_pooled_df,
+        lower.tail = FALSE
+    )
+    group_padjs <- stats::p.adjust(group_pvals, method = p_adjust_method)
+
+    # Critical q-values ====
+    q_crit <- stats::qtukey(
+        p = alpha,
+        nmeans = length(group_names),
+        df = group_pooled_df,
+        lower.tail = FALSE
+    )
+
+    # Confidence interval ====
+    group_confint <- q_crit * group_SE
+    diff_lwr <- group_diff - group_confint
+    diff_upr <- group_diff + group_confint
+
+    # Compact letter display ====
+    group_cld <- compact_letter_display(
+        groups = group_names,
+        means = group_means,
+        comparisons = group_comparisons,
+        pval = group_padjs,
+        alpha = alpha,
+        descending = descending
+    )
+
+    # Output ====
+    desc_df <- data.frame(
+        GROUPS = group_names,
+        N = group_sizes,
+        AVG = group_means,
+        SD = desc_mat[, "sd"],
+        MED = desc_mat[, "median"],
+        MIN = desc_mat[, "min"],
+        MAX = desc_mat[, "max"],
+        CLD = group_cld
+    )
+
+    comparisons_df <- data.frame(
+        comparisons = group_comparisons,
+        diff = group_diff,
+        lwr = diff_lwr,
+        upr = diff_upr,
+        StdErr = group_SE,
+        qval = group_qvals,
+        qcrit = q_crit,
+        pval = group_pvals,
+        padj = group_padjs
+    )
+
+
+    res <- list(
+        result = desc_df,
+        comparisons = comparisons_df
+    )
+
+    return(res)
+}
+
+
+
+
+
+
---
+++ R/utils.R
@@ -0,0 +1,202 @@
+
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+## Check if sample sizes are not equal
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+is_unbalance <- function(data, formula){
+    x_name <- as.character(formula)[3]
+    y_name <- as.character(formula)[2]
+    df0 <- data.frame(
+        x = data[[x_name]],
+        y = data[[y_name]]
+    )
+    n <- unname(with(df0, tapply(y, x, "length")))
+    res <- length(unique(n)) > 1
+    return(res)
+}
+
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+## Check if the data is tied (all values are identical)
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+is_tied <- function(data, formula){
+    if (is.data.frame(data)) {
+        y_name <- as.character(formula)[2]
+        x_name <- as.character(formula)[3]
+
+        df0 <- data.frame(
+            y = data[[y_name]],
+            x = data[[x_name]]
+        )
+
+        df0 <- df0[!is.na(df0$y), ]
+
+        res <- any(with(df0, tapply(y, x, "stats::sd")) == 0)
+    }
+
+    if (is.null(dim(data))){
+        res <- ( length(unique(data)) < length(data) )
+    }
+
+    return(res)
+}
+
+
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+## Check data normality
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+is_normal <- function(data, formula, alpha = 0.05) {
+    #### Dataframe input ====
+    if (is.data.frame(data)) {
+        y_name <- as.character(formula)[2]
+        x_name <- as.character(formula)[3]
+
+        df0 <- data.frame(
+            y = data[[y_name]],
+            x = data[[x_name]]
+        )
+
+        df0 <- df0[!is.na(df0$y), ]
+
+        if (is_tied(df0, y ~ x)) {
+            warning("This is tied-data.")
+            res <- FALSE
+        } else {
+            aov_mod <- stats::aov(formula = y ~ x, data = df0)
+            res <- stats::shapiro.test(aov_mod$residuals)$p.value > alpha
+        }
+    }
+
+    #### Vector input ====
+    if (is.null(dim(data))) {
+        data <- stats::na.omit(data)
+
+        if (is_tied(data)) {
+            warning("This is tied-data.")
+            res <- FALSE
+        } else {
+            res <- stats::shapiro.test(data)$p.value > alpha
+        }
+    }
+
+    return(res)
+}
+
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+## Equal to `outer(X = x1, Y = x1, FUN = function(x1, x2) paste(x1, x2, sep = " "))`
+## applying the `paste` function to the two identical matrices
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+outer2 <- function(x, FUN = "paste"){
+    res <- outer(x, x, FUN)
+    res <- res[upper.tri(res)]
+    return(res)
+}
+
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+## Estimate the compact letter display (CLD) position to show on the plot
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+estimate_letter_pos <- function(x){
+    MAX <- max(x)
+    letter_pos <- MAX + ((ceiling(max(MAX) * 1.15) - max(MAX)) * 0.43)
+    return(letter_pos)
+}
+
+estimate_cld_pos <- estimate_letter_pos
+
+
+# #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# #> Plot a boxplot with compact letter display (cld)
+# #> The cld should be a named vector, for example, c(level_01 = "a", level_02 = "b")
+# show_boxplot <- function(
+#         data,
+#         formula,
+#         cld = NULL,
+#         color = NULL,
+#         point_size = 3,
+#         triangle_size = 3,
+#         letter_size = 8,
+#         letter_pos_adjust = 0,
+#         x_label_order = NULL
+# ){
+#     x_name <- as.character(formula)[3]
+#     y_name <- as.character(formula)[2]
+#
+#     df0 <- data.frame(
+#         x = data[[x_name]],
+#         y = data[[y_name]]
+#     )
+#
+#     if (is.null(x_label_order)) x_label_order <- sort(unique(as.character(df0$x)))
+#     factor_missing <- any(!(x_label_order %in% unique(df0$x)))
+#     if (factor_missing) warning("Factor not match")
+#     df0$x <- factor(df0$x, levels = x_label_order)
+#
+#     if (!is.null(color)) df0$color <- data[[color]]
+#
+#     p1 <- ggplot(df0, aes(x, y, color = color)) +
+#         theme_bw() +
+#         labs(
+#             x = sym(x_name),
+#             y = sym(y_name)
+#         ) +
+#         geom_point(
+#             size = point_size,
+#             position = position_jitter(width = 0.1),
+#             alpha = 0.5,
+#             show.legend = FALSE
+#         ) +
+#         geom_boxplot(
+#             outliers = FALSE,
+#             outlier.colour = "transparent",
+#             outlier.size = 0,
+#             outlier.shape = NA,
+#             fill = NA,
+#             size = 1,
+#             linewidth = 0.5
+#         ) +
+#         stat_summary(
+#             geom = "point",
+#             fun = "mean",
+#             size = triangle_size,
+#             color = "black",
+#             shape = 17,
+#             alpha = 0.7
+#         ) +
+#         theme(
+#             text = element_text(family = "sans", face = "bold", size = 21),
+#             axis.title.x.bottom = element_text(margin = ggplot2::margin(t = 9)),
+#             axis.title.y.left = element_text(margin = ggplot2::margin(r = 9)),
+#             legend.position = "none"
+#         )
+#
+#     if (!is.null(cld)){
+#         cld <- data.frame(
+#             x = names(cld),
+#             letter = unname(cld)
+#         )
+#
+#         letter_pos <- df0 %>%
+#             summarise(
+#                 letter_pos = estimate_letter_pos(y),
+#                 .by = x
+#             ) %>%
+#             left_join(cld, by = "x")
+#
+#         # letter_pos$letter <- unname(cld[match(names(cld), letter_pos[["x"]])])
+#
+#         p1 <- p1  +
+#             geom_text(
+#                 data = letter_pos,
+#                 mapping = aes(x, letter_pos + letter_pos_adjust, label = letter),
+#                 inherit.aes = FALSE,
+#                 size = letter_size
+#             )
+#     }
+#
+#     return(p1)
+# }
+
+
