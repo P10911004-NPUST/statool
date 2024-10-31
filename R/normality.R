ad13cdc70660eb40ce3304a94386fa87a638fa19
jklai <p10911004@gmail.com>
2024-10-31 20:10:15 +0800

    add: Jarque_Bera_test()

--- R/normality.R
+++ R/normality.R
@@ -4,7 +4,7 @@
 #' Skewness of the sample / population distribution
 #'
 #' Return the skewness of the sample / population distribution.
-#' The skewness value for a normal distribution should be close to zero.
+#' The skewness of a normal distribution approximately equal to zero.
 #' A negative skewness value indicates left-skewed distribution (concentrated on the right);
 #' A positive skewness value indicates right-skewed distribution (concentrated on the left).
 #'
@@ -20,6 +20,8 @@
 #' skewness(x)
 #' #> 0.3512426
 skewness <- function(x, .population = FALSE) {
+    if (!is.null(dim(x)) || !is.numeric(x)) stop("x should be a numeric vector")
+
     x <- x[stats::complete.cases(x)]
     N <- length(x)
     if (N < 3 || is.null(N)) stop("Sample size should be greater than 3")
@@ -40,9 +42,11 @@ skewness <- function(x, .population = FALSE) {
 #' Kurtosis of the sample / population distribution
 #'
 #' Return the kurtosis of the sample / population distribution.
+#' Normally distributed data has kurtosis around 3.0.
 #'
 #' @param x A vector of numeric values. NA values will be discarded automatically.
-#' @param .population Calculate the kurtosis of the population or the sample.
+#' @param excess_kurtosis Logical (default: TRUE). Calculate the excess kurtosis (Ke); Ke = K - 3.
+#' @param .population Logical (default: FALSE). Calculate the kurtosis of the population or the sample.
 #'
 #' @return Numeric value
 #' @export
@@ -52,25 +56,30 @@ skewness <- function(x, .population = FALSE) {
 #' x <- stats::rnorm(10, 10)
 #' kurtosis(x)
 #' #> -0.3169031
-kurtosis <- function(x, .population = FALSE) {
+kurtosis <- function(x, excess_kurtosis = TRUE, .population = FALSE) {
+    if (!is.null(dim(x)) || !is.numeric(x)) stop("x should be a numeric vector")
+
     x <- x[stats::complete.cases(x)]
     N <- length(x)
+    if (N < 3 || is.null(N)) stop("Sample size should be greater than 3")
 
     if (.population) {
         ret <- sum( ( (x - mean(x)) / sd_population(x) ) ^ 4 ) / N
     } else {
         # Block A >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        numerator <- (N + 1) * N * sum( (x - mean(x)) ^ 4 )
-        denominator <- (N - 1) * (N - 2) * (N - 3) * ( stats::sd(x) ^ 4 )
-        block_A <- numerator / denominator
+        A_numerator <- (N + 1) * N * sum( (x - mean(x)) ^ 4 )
+        A_denominator <- (N - 1) * (N - 2) * (N - 3) * ( stats::sd(x) ^ 4 )
+        block_A <- A_numerator / A_denominator
 
         # Block B >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        numerator <- 3 * ( (N - 1) ^ 2 )
-        denominator <- (N - 2) * (N - 3)
-        block_B <- numerator / denominator
+        B_numerator <- 3 * ( (N - 1) ^ 2 )
+        B_denominator <- (N - 2) * (N - 3)
+        block_B <- B_numerator / B_denominator
 
         ret <- block_A - block_B
     }
+
+    if (isFALSE(excess_kurtosis)) ret <- ret + 3
 
     return(ret)
 }
@@ -150,7 +159,7 @@ Anderson_Darling_test <- function(x) {
     if (AD >= 10)
         pval <- 9.9e-12
 
-    is_normality <- pval > 0.05
+    is_normality <- pval >= 0.05
 
     ret <- list(
         "is_normality" = is_normality,
@@ -212,8 +221,66 @@ D.Angostino_Pearson_test <- function(x) {
 }
 
 
+#' Jarque-Bera normality test
+#'
+#' Test the data normality (normal distribution)
+#'
+#' @param x A numeric vector
+#'
+#' @return A list with three elements
+#' 1. is_normality: logical value. TRUE: x is normal distribution
+#' 2. statistic: the Jarque-Bera test statistic value.
+#' The higher the value, the lower the probability of normality.
+#' 3. pval: the p-value of the statistics. pval >= 0.05 indicates normal distribution.
+#' @export
+#'
+#' @examples
+#' set.seed(1)
+#' x <- stats::rnorm(100)  # This is normal distribution
+#' Jarque_Bera_test(x)
+#' #> $is_normality
+#' #> [1] TRUE
+#' #> $statistic
+#' #> [1] 0.1103686
+#' #> $pval
+#' #> [1] 0.9463107
+#'
+#' set.seed(1)
+#' x <- stats::rchisq(100, 1)  # This is non-normal distribution
+#' Jarque_Bera_test(x)
+#' #> $is_normality
+#' #> [1] FALSE
+#' #> $statistic
+#' #> [1] 615.3756
+#' #> $pval
+#' #> [1] 2.359915e-134
+#' @references
+#' Jarque, C.M. and Bera, A.K. 1980.
+#' Efficient tests for normality, homoscedasticity and serial independence of regression residuals.
+#' Economics letters, 6(3), 255-259.
 Jarque_Bera_test <- function(x) {
-    cat("Not yet")
+    if (!is.null(dim(x)) || !is.numeric(x)) stop("x should be a numeric vector")
+
+    x <- x[stats::complete.cases(x)]
+    N <- length(x)
+    if (N < 3 || is.null(N)) stop("Sample size should be greater than 3")
+
+    skew <- skewness(x)
+    Ke <- kurtosis(x, excess_kurtosis = TRUE)
+
+    JB <- (N / 6) * ( (skew ^ 2) + ((Ke ^ 2) / 4) )
+
+    pval <- exp(-JB / 2)  # equivalent to `pchisq(JB, 2, lower.tail = FALSE)`
+
+    is_normality <- pval >= 0.05
+
+    ret <- list(
+        "is_normality" = is_normality,
+        "statistic" = JB,
+        "pval" = pval
+    )
+
+    return(ret)
 }
 
 
