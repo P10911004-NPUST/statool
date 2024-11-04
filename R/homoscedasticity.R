0efa2dcad151bbfab1de7ede471e0d1cc4a589fd
jklai <jklai@gate.sinica.edu.tw>
2024-11-04 17:39:34 +0800

    modify: homoscedasticity

--- R/homoscedasticity.R
+++ R/homoscedasticity.R
@@ -1,10 +1,14 @@
 # Test if the data variances are equal (homoscedasticity)
 
-## There are several solutions to test for the equality of variance across groups, including:
-## 1. F-test
-## 2. Barlett's test
-## 3. Levene's test
-## 4. Fligner-Killeen test
+# For grouped data ====
+## F-test
+## Barlett's test
+## Levene's test (including Brown-Forsythe test, when using median or trimmed-mean)
+## Fligner-Killeen test
+## Cochran's C test
+## Conover's test
+## Hartley's test
+## O'Brien's test
 
 
 F_test <- function(data, formula){
@@ -12,28 +16,60 @@ F_test <- function(data, formula){
 }
 
 
+#' Barlett's test
+#'
+#' This is a wrapper for the `stats::bartlett.test()` function.
+#' Computes Barlett's test for homogeneity of variance across groups (homoscedasticity).
+#'
+#' @param data data frame
+#' @param formula formula
+#'
+#' @return returns a list containing three elements:
+#' 1. is_var_equal
+#' 2. statistic
+#' 3. pval
+#' @export
+#' @author Joon-Keat Lai
+#'
+#' @import datasets
+#' @importFrom utils data
+#'
+#' @examples
+#' utils::data("iris", package = "datasets")
+#' barlett_test(iris, Petal.Length ~ Species)
+#' #> $is_var_equal
+#' #> [1] FALSE
+#' #>
+#' #> $statistic
+#' #> Bartlett's K-squared
+#' #>              55.4225
+#' #>
+#' #> $pval
+#' #> [1] 2.005419e-08
+#'
+#' @seealso [stats::bartlett.test()]
 barlett_test <- function(data, formula){
-    # If the data input is a dataframe
-    if (is.data.frame(data)){
-        y_name <- as.character(formula)[2]
-        x_name <- as.character(formula)[3]
-        # res <- bartlett.test(get(y_name) ~ get(x_name), data)
+    if (!is.data.frame(data)) stop("`data` should be a dataframe")
 
-        df0 <- data.frame(
-            y = data[[y_name]],
-            x = data[[x_name]]
-        )
+    df0 <- stats::model.frame(formula = formula, data = data)
+    colnames(df0) <- c("y", "x")
 
-        df0 <- df0[!is.na(df0$x), ]
+    df0 <- df0[stats::complete.cases(df0$x), , drop = FALSE]
 
-        res <- stats::bartlett.test(formula = y ~ x, data = df0)
-    } else{
-        cat("Data input should be a dataframe")
-    }
+    results <- stats::bartlett.test(formula = y ~ x, data = df0)
 
-    return(res$p.value)
+    pval <- results$p.value
+    is_var_equal <- pval > 0.05
+    statistic <- results$statistic
+
+    ret <- list(
+        "is_var_equal" = is_var_equal,
+        "statistic" = statistic,
+        "pval" = pval
+    )
+
+    return(ret)
 }
-
 
 
 
@@ -58,12 +94,14 @@ barlett_test <- function(data, formula){
 #' @examples
 #' utils::data("iris", package = "datasets")
 #' levene_test(iris, Petal.Length ~ Species)
+#'
 #' #> $is_var_equal
 #' #> [1] FALSE
-#' #> $statistic
-#' #> Analysis of Variance Table
 #' #>
-#' #> Response: diff
+#' #> $statistic
+#' #> Analysis of Variance Table (trim_mean)
+#' #>
+#' #> Response: abs(x - trim_mean)
 #' #>            Df Sum Sq Mean Sq F value    Pr(>F)
 #' #> x           2 2.6526 1.32629  20.045 2.005e-08 ***
 #' #> Residuals 147 9.7265 0.06617
@@ -95,14 +133,18 @@ levene_test <- function(data, formula, method = NULL){
 
     aov_mod <- stats::aov(formula = diff ~ x, data = df0)
     aov_mod <- stats::anova(aov_mod)
+    attr(aov_mod, "heading") <- c(
+        sprintf("Analysis of Variance Table (%s)\n", method),
+        sprintf("Response: abs(x - %s)", method)
+    )
 
     pval <- aov_mod$`Pr(>F)`[1]
     is_var_equal <- pval > 0.05
 
     ret <- list(
         "is_var_equal" = is_var_equal,
-        statistic = aov_mod,
-        pval = pval
+        "statistic" = aov_mod,
+        "pval" = pval
     )
 
     return(ret)
@@ -110,27 +152,70 @@ levene_test <- function(data, formula, method = NULL){
 
 
 
-
+#' Fligner's test
+#'
+#' This is a wrapper for the `stats::fligner.test()` function.
+#' Computes Fligner's test for homogeneity of variance across groups (homoscedasticity).
+#'
+#' @param data data frame
+#' @param formula formula
+#'
+#' @return returns a list containing three elements:
+#' 1. is_var_equal
+#' 2. statistic
+#' 3. pval
+#' @export
+#' @author Joon-Keat Lai
+#'
+#' @import datasets
+#' @importFrom utils data
+#'
+#' @examples
+#' utils::data("iris", package = "datasets")
+#' fligner_test(iris, Petal.Length ~ Species)
+#' #> $is_var_equal
+#' #> [1] FALSE
+#' #>
+#' #> $statistic
+#' #> Fligner-Killeen:med chi-squared
+#' #>                        34.70802
+#' #>
+#' #> $pval
+#' #> [1] 2.90569e-08
+#'
+#' @seealso [stats::fligner.test()]
 fligner_test <- function(data, formula){
-    # If the data input is a dataframe
-    if (is.data.frame(data)){
-        y_name <- as.character(formula)[2]
-        x_name <- as.character(formula)[3]
-        # res <- bartlett.test(get(y_name) ~ get(x_name), data)
+    if (!is.data.frame(data)) stop("`data` should be a dataframe")
 
-        df0 <- data.frame(
-            y = data[[y_name]],
-            x = data[[x_name]]
-        )
+    df0 <- stats::model.frame(formula = formula, data = data)
+    colnames(df0) <- c("y", "x")
 
-        df0 <- df0[!is.na(df0$x), ]
+    df0 <- df0[stats::complete.cases(df0$x), , drop = FALSE]
 
-        res <- stats::fligner.test(formula = y ~ x, data = df0)
-    } else{
-        cat("Data input should be a dataframe")
-    }
+    results <- stats::fligner.test(formula = y ~ x, data = df0)
 
-    return(res$p.value)
+    pval <- results$p.value
+    is_var_equal <- pval > 0.05
+    statistic <- results$statistic
+
+    ret <- list(
+        "is_var_equal" = is_var_equal,
+        "statistic" = statistic,
+        "pval" = pval
+    )
+
+    return(ret)
 }
+
+
+
+# For regression ====
+## Goldfeld-Quandt test
+## Park test
+## Glejser test
+## Harrison-McCabe test
+## Breusch-Pagan test
+## White test
+## Cook-Weisberg test
 
 
